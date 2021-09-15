a09227b6811582d74d95ed6a34a2e493e2dbca82
Jared <sauvejared@gmail.com>
2021-09-14 23:04:08 -0400

    Creating header and main nav

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <jared-header></jared-header>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <main-page msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import MainPage from './components/MainPage.vue';
+import JaredHeader from './components/JaredHeader.vue';
+
 
 @Component({
   components: {
-    HelloWorld,
+    MainPage,
+    JaredHeader,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style>
+@import './css/main.css';
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'Caveat', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/JaredHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Header">
+    <div class="Logo">
+      <h4>Jared Sauve</h4>
+    </div>
+    <div class="Nav">
+      <p>nav</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+  },
+})
+export default class Header extends Vue {}
+</script>
+
+<style>
+  .Header{
+    display: flex;
+  }
+  .Logo{
+    font-size: 38px;
+    width: 85%;
+    background: red;
+    padding: 10px;
+    padding-left: 20px;
+  }
+  .Nav{
+    background: blue;
+    padding: 20px;
+    width: 15%;
+  }
+</style>
