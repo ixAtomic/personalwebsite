bf0bc39e5d7fb75ff322d43de00611cf0b86fb9a
ixatomic <sauvejared@gmail.com>
2021-12-14 19:40:55 -0500

    Updating Packages and MainPage

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home-page">
     <div class="middle-justify">
-      <h2>Jared Sauve</h2>
+      <div class="link-content">
+        <h2>Jared Sauve</h2>
+      </div>
+      <div class="main-image">
+        <img src="../../photos/IMG_4161.jpg" />
+      </div>
     </div>
   </div>
 </template>
@@ -16,11 +21,31 @@ export default class HelloWorld extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
   .home-page{
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100%;
+
+    .middle-justify{
+      width: 1000px;
+      height: 600px;
+      display: flex;
+      // flex: 1 0 auto;
+      
+      .link-content{
+        background-color: blue;
+        height: 400px;
+        align-self: center;
+      }
+      .main-image{
+        border-radius: 50%;
+        // background-image: url("../../photos/IMG_4161.jpg");
+        height: 600px;
+        width: 600px;
+      }
+    }
   }
+
 </style>
