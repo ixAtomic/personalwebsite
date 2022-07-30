0ff6c76e9284e542f3bbaebdfd7dd954e843e946
Jared <sauvejared@gmail.com>
2022-07-29 20:06:42 -0400

    build is a string

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 const path = require("path");
 // https://vitejs.dev/config/
 export default defineConfig((command) => {
-  if (command === build) {
+  if (command === "build") {
     return {
       publicDir: "https://jaredsauve.com",
       server: {
