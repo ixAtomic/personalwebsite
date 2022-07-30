451f860a5d355bea09a65ef4dd215e04a1851085
Jared <sauvejared@gmail.com>
2022-07-29 20:03:55 -0400

    Prod config

--- vite.config.js
+++ vite.config.js
@@ -5,38 +5,78 @@ import vue from "@vitejs/plugin-vue";
 
 const path = require("path");
 // https://vitejs.dev/config/
-export default defineConfig({
-  publicDir: "https://personalwebsite-3y4.pages.dev",
-  server: {
-    proxy: {
-      "^/api/*": {
-        target: "https://jared-sauve.herokuapp.com",
-        secure: false,
-        changeOrigin: true,
-      },
-      "^/uploads/*": {
-        target: "https://jared-sauve.herokuapp.com",
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-    port: 8080,
-  },
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 2,
+export default defineConfig((command) => {
+  if (command === build) {
+    return {
+      publicDir: "https://jaredsauve.com",
+      server: {
+        proxy: {
+          "^/api/*": {
+            target: "https://strapi.jaredsauve.com",
+            secure: false,
+            changeOrigin: true,
+          },
+          "^/uploads/*": {
+            target: "https://strapi.jaredsauve.com",
+            secure: false,
+            changeOrigin: true,
           },
         },
+        port: 8080,
       },
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src/"),
-      vue: "@vue/compat",
-    },
-  },
+      plugins: [
+        vue({
+          template: {
+            compilerOptions: {
+              compatConfig: {
+                MODE: 2,
+              },
+            },
+          },
+        }),
+      ],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src/"),
+          vue: "@vue/compat",
+        },
+      },
+    };
+  } else {
+    return {
+      publicDir: "http://localhost:8080",
+      server: {
+        proxy: {
+          "^/api/*": {
+            target: "http://localhost:8090",
+            secure: false,
+            changeOrigin: true,
+          },
+          "^/uploads/*": {
+            target: "http://localhost:8090",
+            secure: false,
+            changeOrigin: true,
+          },
+        },
+        port: 8080,
+      },
+      plugins: [
+        vue({
+          template: {
+            compilerOptions: {
+              compatConfig: {
+                MODE: 2,
+              },
+            },
+          },
+        }),
+      ],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src/"),
+          vue: "@vue/compat",
+        },
+      },
+    };
+  }
 });
