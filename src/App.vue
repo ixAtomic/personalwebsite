af16f13867f7a14de602933661f14c58da95c035
Jared Sauve <sauvejared@gmail.com>
2023-02-06 17:29:18 -0500

    fixing routes

--- src/App.vue
+++ src/App.vue
@@ -10,32 +10,9 @@
 import { configureCompat, watch } from "vue";
 import { RouterView } from "vue-router";
 
-// const { result } = useQuery(gql`
-//   query {
-// 		stuff{
-//       test,
-//       testwysyeyg
-//     }
-//   }
-// `);
-
-// const query = gql`
-//   query {
-//     server_ping
-//   },
-// `;
-
-// const { result } = useQuery(query, null, { clientId: 'System' })
-
-// watch(result, value => {
-//   console.log(value);
-// })
-
 configureCompat({
   WATCH_ARRAY: false,
-  RENDER_FUNCTION: false
 });
-
 </script>
 
 <style>
---
+++ src/global.ts
@@ -0,0 +1,3 @@
+export function htmlGenerator(content: object){
+    
+}
